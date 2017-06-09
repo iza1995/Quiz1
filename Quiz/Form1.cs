bbbf65d7a324b3401ad0a573da334cf92687810d
iza1995 <izabela_jackowska@o2.pl>
2017-06-09 20:54:15 +0200

    Dodano wyswietlanie tresci pytania, odpowiedzi oraz obrazek

--- Quiz/Form1.cs
+++ Quiz/Form1.cs
@@ -10,11 +10,55 @@ using System.Windows.Forms;
 
 namespace Quiz
 {
+
+
     public partial class Form1 : Form
     {
+        private const int liczbaPytan = 20;
+        private string[] pytania = new string[liczbaPytan];
+        private string[] odpowiedziA = new string[liczbaPytan];
+        private string[] odpowiedziB = new string[liczbaPytan];
+        private string[] odpowiedziC = new string[liczbaPytan];
+        private string[] odpowiedziD = new string[liczbaPytan];
+        private string[] poprawnaOdpowiedz = new string[liczbaPytan];
+        private string[] obraz = new string[liczbaPytan];
+
+
+        public void uzupelnij()
+        {
+            for (int i = 0; i < liczbaPytan; i++)
+            {
+                pytania[i] = "pytanie pytanie pytanie pytanie pytanie" + i + "\n" + "pytanie pytanie pytanie pytanie pytanie" + "\n" + "pytanie pytanie pytanie pytanie pytanie";
+                odpowiedziA[i] = "Odpowiedz A dla pytania numer numernumernumernumernumernumernumernumernumernumernumernumernumernumernumernumernumernumer " + i;
+                odpowiedziB[i] = "Odpowiedz B dla pytania numer numernumernumernumernumernumernumernumernumernumernumernumernumernumernumernumernumernumer " + i;
+                odpowiedziC[i] = "Odpowiedz C dla pytania numer numernumernumernumernumernumernumernumernumernumernumernumernumernumernumernumernumernumer " + i;
+                odpowiedziD[i] = "Odpowiedz D dla pytania numer numernumernumernumernumernumernumernumernumernumernumernumernumernumernumernumernumernumer " + i;
+                poprawnaOdpowiedz[i] = "B";
+                if (i % 2 == 0)
+                {
+                    obraz[i] = "test1.png";
+                    poprawnaOdpowiedz[i] = "B";
+                }
+                else
+                {
+                    obraz[i] = "01.png";
+                    poprawnaOdpowiedz[i] = "C";
+                }
+
+            }
+
+            label1.Text = pytania[1];
+            button1.Text = odpowiedziA[1];
+            button2.Text = odpowiedziB[1];
+            button3.Text = odpowiedziC[1];
+            button4.Text = odpowiedziD[1];
+            pictureBox1.Image = new Bitmap(@"C:\Users\Konto\Desktop\Quiz\Quiz\Quiz\" + obraz[1]);
+        }
+
         public Form1()
         {
             InitializeComponent();
+            uzupelnij();
         }
 
         private void button1_Click(object sender, EventArgs e)
